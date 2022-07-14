e1ff862d9d28bea5fd69bac1ba0870e0a26bda19
Ololade Olanrewaju <Oolarenwaju@wragbysolutions.com>
2022-07-14 14:35:23 +0100

    header

--- monty.h
+++ monty.h
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include <stdbool.h>
-#include <ctype.h>
 
 /**
  * struct globals - variables globales
