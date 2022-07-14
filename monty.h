983f2bf66aefdb25b9005787fb1b65d8d3fdc5a0
Ololade Olanrewaju <49099449+lolade-dev@users.noreply.github.com>
2022-07-14 13:55:51 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -26,6 +26,8 @@ typedef struct arg_s
 	int flag;
 } arg_t;
 
+extern arg_t arg;
+
 /**
   * struct stack_s - doubly linked list representation of a stack (or queue)
   * @n: integer element within node.
