b2a942fce8d56cbabb0782038fc70d79245ae306
Ololade Olanrewaju <Oolarenwaju@wragbysolutions.com>
2022-07-14 15:29:08 +0100

    monty

--- find_opcode.c
+++ find_opcode.c
@@ -12,24 +12,24 @@ void (*func(char *tokens))(stack_t **stack, unsigned int num_line)
 {
 	instruction_t ops[] = {
 		{"push", _push},
-		{"pall", _pall},
-		{"pint", _pint},
-		{"pop", _pop},
-		{"swap", _swap},
-		{"add", _add},
-		{"nop", _nop},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
 		{"sub", _sub},
 		{"div", _div},
 		{"mul", _mul},
 		{"mod", _mod},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
+		{"pchar", pchar},
+		{"pstr", pstr},
 		{"rotl", _rotl},
 		{"rotr", _rotr},
 		{"stack", _stack},
 		{"queue", _queue},
 		{"err", _error},
-		{"#", _nop},
+		{"#", nop},
 		{NULL, NULL}};
 	int i = 0;
 	int opc;
